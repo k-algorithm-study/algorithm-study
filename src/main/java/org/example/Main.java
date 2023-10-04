c1ca4497ef2e009e06d3f02b67c5f06fba791208
yumyeonghan <mgtmh991013@gmail.com>
2023-10-04 17:41:44 +0900

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/***
+ * class 바로 위에 <문제 이름, 난이도>
+ * class 명은 <패키지 명 + 번호>
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/org/example/programmers/Programmers01.java
@@ -0,0 +1,4 @@
+package org.example.programmers;
+
+public class Programmers01 {
+}
