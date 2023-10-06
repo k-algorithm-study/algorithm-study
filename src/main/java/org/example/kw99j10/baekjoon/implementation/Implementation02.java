56ad4cacabc2698521ce8e0fa063030c928be352
Jung kyoungwon <foryou0084@gmail.com>
2023-10-06 11:20:03 +0900

    Implementation- 그룹 단어 체커, Silver V

---
+++ src/main/java/org/example/kw99j10/baekjoon/implementation/Implementation02.java
@@ -0,0 +1,13 @@
+package org.example.kw99j10.baekjoon.implementation;
+
+import java.util.Scanner;
+
+/**
+ * 셀프 넘버 - Silver V
+ */
+public class Implementation02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
