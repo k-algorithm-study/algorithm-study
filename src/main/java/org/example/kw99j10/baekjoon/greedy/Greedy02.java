3c3ef479519f22af3834ead94a434c42b3dc528a
Jung kyoungwon <foryou0084@gmail.com>
2023-10-07 14:19:35 +0900

    Greedy- 설탕 배달, Silver IV

---
+++ src/main/java/org/example/kw99j10/baekjoon/greedy/Greedy02.java
@@ -0,0 +1,13 @@
+package org.example.kw99j10.baekjoon.greedy;
+
+import java.util.Scanner;
+
+/**
+ * 동전 0 - Silver IV
+ */
+public class Greedy02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
