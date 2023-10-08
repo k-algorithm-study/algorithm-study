4dddd8d47059c858603eeb0ced9f163a83b84626
yumyeonghan <mgtmh991013@gmail.com>
2023-10-08 14:14:38 +0900

    :sparkles: 두 원 사이의 정수 쌍, 레벨2

---
+++ src/main/java/org/example/yumyeonghan/programmers/level2/두원사이의정수쌍.java
@@ -0,0 +1,18 @@
+package org.example.yumyeonghan.programmers.level2;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181187
+public class 두원사이의정수쌍 {
+
+    public long solution(int r1, int r2) {
+        //x^2 + y^2 = r^2
+        long answer = 0;
+        for (int i = 1; i <= r2; i++) {
+            long r2Pair = (long) Math.floor(Math.sqrt(1.0 * r2 * r2 - 1.0 * i * i));
+            long r1Pair = (long) Math.ceil(Math.sqrt(1.0 * r1 * r1 - 1.0 * i * i)); // 음수의 제곱근은 0
+            answer += r2Pair - r1Pair + 1;
+        }
+        return answer * 4;
+    }
+
+}
+
