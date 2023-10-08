f8b6015a52384c80ae85766ea26eb6b181e96e6c
Jung kyoungwon <foryou0084@gmail.com>
2023-10-09 02:00:43 +0900

    Programmers- 두 원 사이의 정수 쌍, lv 2

---
+++ src/main/java/org/example/kw99j10/programmers/lv2/요격시스템.java
@@ -0,0 +1,11 @@
+package org.example.kw99j10.programmers.lv2;
+
+/**
+ *  요격 시스템, programmers:Level 2
+ */
+public class 요격시스템 {
+    public int solution(int[][] targets) {
+        int answer = 0;
+        return answer;
+    }
+}
