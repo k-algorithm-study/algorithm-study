7b8ff8dbb762b212b5acc3fb1ccede4d9dc9ef80
yumyeonghan <mgtmh991013@gmail.com>
2023-11-17 11:44:11 +0900

    :sparkles:수들의 합, 실버5

---
+++ src/main/java/org/example/yumyeonghan/baekjoon/greedy/수들의합.java
@@ -0,0 +1,29 @@
+package org.example.yumyeonghan.baekjoon.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+// https://www.acmicpc.net/problem/1789
+public class 수들의합 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    long S = Long.parseLong(br.readLine());
+    int n = 0;
+    int index = 0;
+    long sum = 0L;
+    while (true) {
+      index++;
+      sum += index;
+      // 순차적으로 더해나갔는데 만약 S를 넘어가면 해당 n 반환 => 언제든지 n개의 숫자로 원하는 숫자를 만들 수 있으므로
+      if (sum > S) {
+        break;
+      }
+      n++;
+    }
+    System.out.println(n);
+  }
+
+}
