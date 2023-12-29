36a9b4243bf9959a0e03236af834b281c2d4c84c
ChanUi <hcu55@naver.com>
2023-12-29 16:08:25 +0900

    알파벳(graph), 골드 IV

--- src/main/java/org/example/hcu55/baekjoon/graph/알파벳.java
+++ src/main/java/org/example/hcu55/baekjoon/graph/알파벳.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class 알파벳 {
     static int r, c;
     static int[][] arr;
-    static boolean[] checked;
+    static boolean[] visited;
     static int max = 0;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
@@ -17,31 +17,31 @@ public class 알파벳 {
         c = sc.nextInt();
 
         arr = new int[r][c];
-        checked = new boolean[26];      // 알파벳 26개
+        visited = new boolean[26];      // 알파벳 26개
         for (int i = 0; i < r; i++) {
             String input = sc.next();
             for (int j = 0; j < c; j++) {
                 arr[i][j] = input.charAt(j) - 'A';
             }
         }
-        dfs(0, 0, 0);
+        dfs(0, 0, 1);
         System.out.println(max);
     }
 
-    static void dfs(int x, int y, int cnt) {
-        if (checked[arr[x][y]]) {
-            max = Math.max(max, cnt);
-        } else {
-            checked[arr[x][y]] = true;
-            for (int i = 0; i < 4; i++) {
-                int nextX = x + dx[i];
-                int nextY = y + dy[i];
+    public static void dfs(int x, int y, int cnt) {
+        visited[arr[x][y]] = true;
+        max = Math.max(max, cnt);
 
-                if (nextX >= 0 && nextY >= 0 && nextX < r && nextY < c) {
+        for (int i = 0; i < 4; i++) {
+            int nextX = x + dx[i];
+            int nextY = y + dy[i];
+
+            if (nextX >= 0 && nextY >= 0 && nextX < r && nextY < c) {
+                if (visited[arr[nextX][nextY]] == false) {      // 방문하지 않은 곳
                     dfs(nextX, nextY, cnt + 1);
+                    visited[arr[nextX][nextY]] = false;         // 백트레킹으로 다시 미방문 처리
                 }
             }
-            checked[arr[x][y]] = false;
         }
     }
 }
