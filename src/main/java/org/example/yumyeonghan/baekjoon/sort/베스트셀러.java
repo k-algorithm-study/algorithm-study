3785ef58702884d7520b4d3613e931138fb2211f
yumyeonghan <mgtmh991013@gmail.com>
2024-01-11 03:29:32 +0900

    refactor: optimize imports

--- src/main/java/org/example/yumyeonghan/baekjoon/sort/베스트셀러.java
+++ src/main/java/org/example/yumyeonghan/baekjoon/sort/베스트셀러.java
@@ -1,12 +1,9 @@
 package org.example.yumyeonghan.baekjoon.sort;
 
-import com.sun.source.tree.Tree;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
