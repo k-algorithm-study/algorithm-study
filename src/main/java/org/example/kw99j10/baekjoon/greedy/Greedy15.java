0446166e763871e299dd89340221993ea59fc128
Jung kyoungwon <foryou0084@gmail.com>
2024-03-29 19:08:48 +0900

    Greedy - 행렬, Silver I

---
+++ src/main/java/org/example/kw99j10/baekjoon/greedy/Greedy15.java
@@ -0,0 +1,64 @@
+package org.example.kw99j10.baekjoon.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ *  행렬 - Silver I
+ */
+public class Greedy15 {
+    static int[][] a;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        a = new int[n][m];
+        int[][] b = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < m; j++) {
+                a[i][j] = s.charAt(j) - '0';
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < m; j++) {
+                b[i][j] = s.charAt(j) - '0';
+            }
+        }
+
+        int answer = 0;
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = 0; j < m - 2; j++) {
+                if (a[i][j] != b[i][j]) {
+                    change(i, j);
+                    answer++;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (a[i][j] != b[i][j]) {
+                    answer = -1;
+                    break;
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+
+    static void change(int x, int y) {
+        for (int i = x; i < x + 3; i++) {
+            for (int j = y; j < y + 3; j++) {
+                a[i][j] = a[i][j] == 0 ? 1 : 0;
+            }
+        }
+    }
+}
